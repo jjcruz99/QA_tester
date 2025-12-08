297fde4cba5996b386b603820fa518a113a3f958
jjcruz99 <jjcruz139@gmail.com>
2025-12-07 21:35:08 -0500

    generacion de reportes con cypress

--- pruebas_automatizadas/SITIO_PRUEBAS/sitio_pruebas/cypress.config.js
+++ pruebas_automatizadas/SITIO_PRUEBAS/sitio_pruebas/cypress.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'cypress';
-import cypressMochawesomeReporter from 'cypress-mochawesome-reporter/plugin.js';
-
-
+import { defineConfig } from "cypress";
+import cypressMochawesomeReporter from "cypress-mochawesome-reporter/plugin.js";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // implement node event listeners here
       cypressMochawesomeReporter(on);
+
     },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: true,
-      charts: true,
-      embeddedScreenshots: true,
-      html: true,
-      json: false,
-      reportPageTitle: 'Pruebas Cypress',
-      inlineAssets: true,
-    },
+     reporter: "cypress-mochawesome-reporter",
+      reporterOptions: {
+        reportDir: "cypress/reports",
+        overwrite: true,
+        charts: true, // graficos en el reporte
+        embedScreenshots: true, // capturas de pantalla en el reporte
+        html: true, // genera reporte en HTML
+        json: false, // no genera reporte en JSON
+        reportPageTitle: "Pruebas Automatizadas con Cypress",
+        inlineAssets: true,
+      },
   },
 });
--- pruebas_automatizadas/SITIO_PRUEBAS/sitio_pruebas/cypress/e2e/02/02_01_estructura_de_test.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Página principal', () => {
-
-    it('Abre la versión en español', () => {
-        cy.visit('http://localhost:5173/');
-        cy.url().should('include', 'localhost')
-    })
-
-})
